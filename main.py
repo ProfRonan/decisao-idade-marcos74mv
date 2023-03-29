1e236abe3f81ea6827dc4df8c12e9e07ee406bf9
Marcos Vinicius <128155251+marcos74mv@users.noreply.github.com>
2023-03-29 14:42:33 +0000

    decisao idade atividade 11

---
+++ main.py
@@ -0,0 +1,23 @@
+print ("Digite um número: ")
+idade = int(input("> "))
+
+impossível = False
+menor = False
+maior =  False
+velho = False
+
+if idade < 0:
+    print("impossível!")
+    impossível = True
+if idade < 18:
+    print ("não precisa se alistar.")
+    menor = True
+if 18 < idade < 65:
+    print ("Não esqueça de votar na próxima eleição")
+    maior = True
+if idade > 65:
+    print ("Vá descansar!")
+    velho = True
+
+if not (impossível or menor or maior or velho):
+    print ("eita!")
