6e1905c961ba032fdb52f2ccc7ebe1c91f21c559
Marcos Vinicius <128155251+marcos74mv@users.noreply.github.com>
2023-05-22 14:31:41 +0000

    exercicio idade corrigido

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ velho = False
 if idade < 0:
     print("Impossível!")
     impossível = True
-if idade < 18:
+elif idade < 18:
     print ("Não precisa se alistar.")
     menor = True
-if 18 < idade < 65:
-    print ("Não esqueça de votar na próxima eleição")
+elif 18 < idade < 65:
+    print ("Não esqueça de votar na próxima eleição.")
     maior = True
-if idade > 65:
+elif idade > 65:
     print ("Vá descansar.")
     velho = True
 
